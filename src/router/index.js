fd15cd0e74b32dd012e821b4ab48af39225895b3
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2022-02-19 22:14:35 +0600

    #12 Vuejs | Route Secure

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store';
 import Login from '../views/auth/Login.vue'
 import Register from '../views/auth/Register.vue'
 import Home from '../views/pages/Home.vue'
@@ -7,19 +8,22 @@ const routes = [
   {
     path: '/',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: { guest: true }   //if authenticate, will never access this
   },
 
   {
     path: '/register',
     name: 'Register',
-    component: Register
+    component: Register,
+    meta: { guest: true }   //if authenticate, will never access this
   },
   
   {
     path: '/admin/home',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: { requiresAuth: true }      //for --> only authenticated users can access this.
   },
   
 ]
@@ -27,6 +31,30 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+// eslint-disable-next-line no-unused-vars
+router.beforeEach((to, from) => {
+  // instead of having to check every route record with
+  // to.matched.some(record => record.meta.requiresAuth)
+  if (to.meta.requiresAuth && !store.getters.GET_AUTH_STATUS) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    return {
+      path: '/',
+      // save the location we were at to come back later
+      query: { redirect: to.fullPath },
+    }
+  }
+  else if (to.meta.guest && store.getters.GET_AUTH_STATUS) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    return {
+      path: '/admin/home',
+      // save the location we were at to come back later
+      query: { redirect: to.fullPath },
+    }
+  }
 })
 
 export default router
